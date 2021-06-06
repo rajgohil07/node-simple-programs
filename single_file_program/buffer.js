e57a9e54083b1da44c0adeca0fedd951107eb56d
rajgohil07 <raj.famous007@gmail.com>
2021-06-06 13:12:30 +0530

    try attempt

--- single_file_program/buffer.js
+++
@@ -1,5 +0,0 @@
-//buffer 1
-let buf = Buffer.alloc(10);
-let rand_str2 = "raj gohil";
-
-console.log(buf.write(rand_str2));
--- streams/pipe.js
+++
@@ -1,8 +0,0 @@
-var fs = require("fs");
-var RS = fs.createReadStream('input.txt');
-var WS = fs.createWriteStream('output.txt');
-
-// Pipe operation
-RS.pipe(WS);
-
-console.log("program invoked!");
