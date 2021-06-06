d8b36c3fdccaddb49668c562dd59b8eae50f219b
rajgohil07 <raj.famous007@gmail.com>
2021-06-06 13:01:13 +0530

    delete all files

--- I-O operation/file_read_async.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-
-fs.readFile("text_input.txt", function (err, result) {
-  err ? console.log("error cause of:" + err) : "";
-  console.log("data from input file :" + result);
-  console.log("\nprogram eneded! inside callback function!");
-});
-console.log("program eneded! after function!\n");
